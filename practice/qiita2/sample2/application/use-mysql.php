9db74b2a03f66e58d6d5e03475d7f3b3383c22de
yinm <yinm.cminor9@gmail.com>
2017-05-05 17:31:40 +0900

    データを永続化できるようにする

--- practice/qiita2/sample2/application/use-mysql.php
+++ practice/qiita2/sample2/application/use-mysql.php
@@ -1,12 +1,12 @@
 <?php
 
-$dsn = 'mysql:host=' . $_ENV['DB_HOST'];
+$dsn = 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=docker_sample';
 $user = 'root';
 $password = $_ENV['DB_PASS'];
 
 $dbh = new PDO($dsn, $user, $password);
 
-$sql = "select version();";
+$sql = "select database();";
 
 foreach ($dbh->query($sql, PDO::FETCH_ASSOC) as $row) {
     print_r($row);
